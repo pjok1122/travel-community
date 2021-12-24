46c32622bc235c4999baff7021afd59443a4e3e1
leehani <rorean@naver.com>
2021-12-24 15:38:36 +0900

    bookmark refactoring

--- src/main/java/project/board/jpa/BookmarkRepositoryJpa.java
+++ src/main/java/project/board/jpa/BookmarkRepositoryJpa.java
@@ -5,11 +5,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import project.board.entity.Article;
 import project.board.entity.Bookmark;
 import project.board.entity.Member;
+
+import java.util.Optional;
 
 public interface BookmarkRepositoryJpa extends JpaRepository<Bookmark, Long> {
 
     @EntityGraph(attributePaths = {"article"})
     Page<Bookmark> findByMember(Member member, Pageable pageable);
+
+    Optional<Bookmark> findByMemberAndArticle(Member member, Article article);
 }
